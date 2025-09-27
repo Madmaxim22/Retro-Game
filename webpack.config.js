6413d01e81b8efddf56f861c1a90b8e1e9e58afe
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-09-28 00:06:31 +0300

    impl: configuration css and static png webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,44 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => {
+
+  const isDev = env.mode === 'development';
+  const isProd = env.mode === 'production';
+
   return {
     mode: env.mode ?? "development",
     entry: path.resolve(__dirname, "src", "index.js"),
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",
+      assetModuleFilename: 'images/[hash][ext][query]',
       clean: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, "public", "index.html"),
+        template: path.resolve(__dirname, "src", "index.html"),
+      }), 
+      isProd && new MiniCssExtractPlugin({
+        filename: 'css/[name].[contenthash:8].css',
+        chunkFilename: 'css/[name].[contenthash:8].css',
       }),
     ],
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: [
+            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
+            "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        }
+      ]
+    },
     devServer: {
       port: 5000,
       open: true
