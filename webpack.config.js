0241ed1036aef8cb2e4ab7fb2b135e4f4e41f8a9
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-09-28 15:11:39 +0300

    impl: change webpack config on ES module

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = (env) => {
-  const isDev = env.mode === 'development';
-  const isProd = env.mode === 'production';
+import path from "path";
+import { fileURLToPath } from "url";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default (env) => {
+  const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
 
   return {
-    mode: env.mode ?? 'development',
-    entry: path.resolve(__dirname, 'src', 'index.js'),
+    mode: env.mode ?? "development",
+    entry: path.resolve(__dirname, "src", "index.js"),
     output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: '[name].[contenthash].js',
-      assetModuleFilename: 'images/[hash][ext][query]',
+      path: path.resolve(__dirname, "build"),
+      filename: "[name].[contenthash].js",
+      assetModuleFilename: "images/[hash][ext][query]",
       clean: true,
     },
     plugins: [
-      new HtmlWebpackPlugin({ template: path.resolve(__dirname, 'src', 'index.html'), }),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src", "index.html"),
+      }),
       isProd &&
         new MiniCssExtractPlugin({
-          filename: 'css/[name].[contenthash:8].css',
-          chunkFilename: 'css/[name].[contenthash:8].css',
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
         }),
     ],
     module: {
@@ -28,21 +37,24 @@ module.exports = (env) => {
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
+          resolve: {
+            fullySpecified: false,
+          },
           use: {
-            loader: 'babel-loader',
-            options: { presets: [ '@babel/preset-env' ], },
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env"] },
           },
         },
         {
           test: /\.css$/i,
           use: [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            'css-loader',
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
           ],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
+          type: "asset/resource",
         },
       ],
     },
