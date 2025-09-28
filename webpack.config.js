ed492e3472ebf9b2da1c1bc600f26aa65bb6e358
Максим Мухомедьяров <madmaxim22@yandex.ru>
2025-09-28 14:10:25 +0300

    impl: configuration babel-loader webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,9 +3,8 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env) => {
-
-  const isDev = env.mode === 'development';
-  const isProd = env.mode === 'production';
+  const isDev = env.mode === "development";
+  const isProd = env.mode === "production";
 
   return {
     mode: env.mode ?? "development",
@@ -13,35 +12,47 @@ module.exports = (env) => {
     output: {
       path: path.resolve(__dirname, "build"),
       filename: "[name].[contenthash].js",
-      assetModuleFilename: 'images/[hash][ext][query]',
+      assetModuleFilename: "images/[hash][ext][query]",
       clean: true,
     },
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "src", "index.html"),
-      }), 
-      isProd && new MiniCssExtractPlugin({
-        filename: 'css/[name].[contenthash:8].css',
-        chunkFilename: 'css/[name].[contenthash:8].css',
       }),
+      isProd &&
+        new MiniCssExtractPlugin({
+          filename: "css/[name].[contenthash:8].css",
+          chunkFilename: "css/[name].[contenthash:8].css",
+        }),
     ],
     module: {
       rules: [
         {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+        {
           test: /\.css$/i,
           use: [
-            isProd ? MiniCssExtractPlugin.loader : 'style-loader',
-            "css-loader"],
+            isProd ? MiniCssExtractPlugin.loader : "style-loader",
+            "css-loader",
+          ],
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        }
-      ]
+          type: "asset/resource",
+        },
+      ],
     },
     devServer: {
       port: 5000,
-      open: true
-    }
+      open: true,
+    },
   };
 };
